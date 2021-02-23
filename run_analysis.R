64d2b0aced0452a02039d04a493ed0d1be50041d
cdjo91 <77229283+cdjo91@users.noreply.github.com>
2021-02-23 12:07:55 +0900

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,88 +1,90 @@
-## This is Getting and Cleaning Data Course Project Assignment
+library(dplyr)
+library(stringr)
 
 
-# Unzip dataSet to /data directory
-unzip(zipfile="./data/Dataset.zip", exdir="./data")
+## 0. Check the directory and files
+getwd()
+list.files()
 
+## 1. Merges the training and the test sets to create one data set.
+# Read the name of the variables
+features <- read.table("./UCI HAR Dataset/features.txt")
 
-#define the path where the new folder has been unziped
-pathdata = file.path("./data", "UCI HAR Dataset") # "file name" 
+# Read the training data
+X_train <- tbl_df(read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features[, 2]))
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "activity_labels_index")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
 
+# Add the activity label and subject label in the training data
+interim_train <- cbind(y_train, X_train)
+X_train_merge <- cbind(subject_train, interim_train)
 
-#create a file which has the 28 file names
-files = list.files(pathdata, recursive = T)
+# Read the test data
+X_test <- tbl_df(read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features[, 2]))
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "activity_labels_index")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
 
+#  Add the activity label and subject label in the test data
+interim_test <- cbind(y_test, X_test)
+X_test_merge <- cbind(subject_test, interim_test)
 
-#show the files
-files
+# Merge the training and the test sets to create one data set
+train_test <- tbl_df(rbind(X_train_merge, X_test_merge))
 
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+extract <- grepl("mean.)|std.)", features[, 2])
+extract_2 <- c(TRUE, TRUE, extract)
+tt_extract <- train_test[, extract_2]
 
-## Step 1. Creating the data set of training and test
+## 3. Uses descriptive activity names to name the activities in the data set
+# Read the activity index and name
+activity <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("activity_labels_index", "activity_labels"))
 
-#Reading training tables - xtrain / ytrain, subject train
-xtrain <- read.table(file.path(pathdata, "train", "X_train.txt"), header = F)
-ytrain <- read.table(file.path(pathdata, "train", "y_train.txt"), header = F)
-subject_train = read.table(file.path(pathdata, "train", "subject_train.txt"), header = F)
+# Uses descriptive activity names
+activity[,2] <- sub("WALKING_", "", activity[, 2])
+activity[,2] <- str_sub(activity[, 2], start = 1, end = 3)
 
+# Change from the activity index to activity name 
+tt_extract_2 <- merge(tt_extract, activity, by = "activity_labels_index")
+tt_extract_3 <- tbl_df(select(tt_extract_2, subject, activity_labels, tBodyAcc.mean...X :fBodyBodyGyroJerkMag.std..))
 
-#Reading the testing tables
-xtest <- read.table(file.path(pathdata, "test", "X_test.txt"), header = F)
-ytest <- read.table(file.path(pathdata, "test", "y_test.txt"), header = F)
-subject_test <- read.table(file.path(pathdata, "test", "subject_test.txt"), header = F)
+# Arrange the data so that subject gets ascending order
+tt_extract_3 <- arrange(tt_extract_3, subject)
 
+## 4. Appropriately labels the data set with descriptive variable names. 
+var_org <- features[extract, 2]
+var_desc <- gsub("Body", "B", var_org)
+var_desc <- gsub("Gravity", "Gr", var_desc)
+var_desc <- gsub("Acc", "A", var_desc)
+var_desc <- gsub("mean.)", "m", var_desc)
+var_desc <- gsub("std.)", "s", var_desc)
+var_desc <- gsub("Jerk", "J", var_desc)
+var_desc <- gsub("Gyro", "Gy", var_desc)
+var_desc <- gsub("Mag", "M", var_desc)
+var_desc <- gsub("\\-", "", var_desc)
+colnames(tt_extract_3) <- c("subject", "activity_labels", var_desc)
 
-#Read the features data
-features <- read.table(file.path(pathdata, "features.txt"), header = F)
+## 5. From the data set in step 4, creates a second, independent tidy data set
+##    with the average of each variable for each activity and each subject.
 
+# average for the groub by the activities
+summarise_activity <-
+  tt_extract_3 %>%
+  group_by(activity_labels) %>%
+  summarise_each_(funs = mean, vars = var_desc)
 
-#Read activity labels data
-activityLabels <- read.table(file.path(pathdata, "activity_labels.txt"), header = F)
+# average for the groub by the subjects
+summarise_subject <-
+  tt_extract_3 %>%
+  group_by(subject) %>%
+  summarise_each_(funs = mean, vars = var_desc)
 
+# Rename the variable name to merge
+summarise_activity <- rename(summarise_activity, ave_by = activity_labels)
+summarise_subject <- rename(summarise_subject, ave_by = subject)
 
-#Create Sanity and Column Values to the Train Data
-colnames(xtrain) <- features[,2]
-colnames(ytrain) <- "activityId"
-colnames(subject_train) <- "subjectId"
+# Merge the 2 data set to submit as one data
+submission <- rbind(summarise_activity, summarise_subject)
 
-
-#Create Sanity and column values to the test data
-colnames(xtest) <- features[,2]
-colnames(ytest) <- "activityId"
-colnames(subject_test) <- "subjectId"
-
-
-#Create sanity check for the activity labels value
-colnames(activityLabels) <- c('activityId','activityType')
-
-
-#Merging the train and test data 
-mrg_train = cbind(ytrain, subject_train, xtrain)
-mrg_test = cbind(ytest, subject_test, xtest)
-
-
-#Create the main data table merging both table tables 
-setAllInOne = rbind(mrg_train, mrg_test)
-
-
-# Need step is to read all the values that are available
-colNames = colnames(setAllInOne)
-
-#Need to get a subset of all the mean and standards and the correspondongin activityID and subjectID 
-mean_and_std = (grepl("activityId" , colNames) | grepl("subjectId" , colNames) | grepl("mean.." , colNames) | grepl("std.." , colNames))
-
-
-#A subtset has to be created to get the required dataset
-setForMeanAndStd <- setAllInOne[ , mean_and_std == T]
-
-setWithActivityNames = merge(setForMeanAndStd, activityLabels, by='activityId', all.x=T)
-
-# New tidy set has to be created 
-secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
-secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
-
-#The last step is to write the output to a text file 
-write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
-
-
-secTidySet
-
+# Output the data set for the submission
+write.table(submission, "submission.txt", row.names =  FALSE)
